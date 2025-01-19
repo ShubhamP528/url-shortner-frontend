5c89945b3bf0ec96814cda2a8e175659108a2623
Shubham Prajapati <shubham528prajapati@gmail.com>
2025-01-19 09:11:34 +0530

    new update

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,2 +1,4 @@
 export const NODE_END_POINT =
-  import.meta.env === "production" ? "" : "http://localhost:8900/api";
+  import.meta.env === "production"
+    ? "https://url-shortner-backend-sigma.vercel.app/api"
+    : "http://localhost:8900/api";
